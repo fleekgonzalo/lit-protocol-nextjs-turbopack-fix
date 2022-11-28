b7a703d1a801c80706fa13f8f35187658b9b491a
ChristianOConnor <47836708+ChristianOConnor@users.noreply.github.com>
2022-11-28 18:21:41 -0500

    back to pre turbopack (#1)

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
-  experimental: {
-    // Required:
-    appDir: true,
-  },
-};
+  reactStrictMode: true,
+  swcMinify: true,
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
